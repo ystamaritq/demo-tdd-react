0d5bc768f7ba1594fb6fc3f0895093a196fc3093
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-18 22:09:26 -0500

    added somexamples

--- src/components/__tests__/Calculator.test.js
+++ src/components/__tests__/Calculator.test.js
@@ -5,7 +5,8 @@ import React from "react";
 import { render, fireEvent, waitFor, screen } from "@testing-library/react";
 // add custom jest matchers from jest-dom
 import "@testing-library/jest-dom/extend-expect";
-import Math from "./../Math";
+import Math from "./../Calculator";
+import Calculator from "./../Calculator";
 
 // the test is async because we can predict the behaviour of the lifecycle methods (re-render methods)
 // recommend time frame for running the test
@@ -15,20 +16,18 @@ import Math from "./../Math";
 
 // 1) wrap all test in 1 describe block
 // 2) add another test
-describe("Math", () => {
-	test("loads and displays greeting", async () => {
+describe("Calculator", () => {
+	test("display a div with a p", async () => {
 		// Arrange your test
-		const { container, asFragment, debug } = render(
-			<Math num1={19} operator="+" num2={341} />
-		);
-		debug();
+		const { container, asFragment, debug } = render(<Calculator />);
+		// debug();
 		// Act your test - the user is not interacting with this
 
 		// Assert your test (a test can have more than one assertion)
 
 		// if something stop in code is because its throw an error
 		// toBe is call the matcher
-		expect(container).toContainHTML(`<span>360</span>`);
+		expect(container.find("div").length).toEqual(1);
 	});
 
 	// calculator
